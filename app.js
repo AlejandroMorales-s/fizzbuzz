bebf098aaa33747cec844004e0e06a9e1b5ea6b6
Alejandro <alejandrom.am33@gmail.com>
2022-04-27 18:19:55 -0500

    add: filtro por mision, obtener username y cantidad de explorers en una mision refactorizados

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const fs = require("fs");
+const Reader = require("./lib/utils/reader");
+const ExplorerService = require("./lib/services/explorerService");
 
 //* Part 1 Read json file ===========================
 const explorers = Reader.readJsonFile("explorers.json");
 //console.log(explorers); 
 
 //* Part 2: Get the quantity of explorers names in node
-const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-//console.log(explorersInNode.length)
+const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+//console.log(explorersInNode)
+
+//* Part 3: Get the amount of explorers by mission
+const amountOfExplorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+console.log(amountOfExplorersInNode); 
 
 //* Part4: Get the explorer's usernames in Node
-const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
-const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
-//console.log(usernamesInNode)
+const usernamesInNode = ExplorerService.getExporersUsernameByMission(explorers, "node");
+console.log(usernamesInNode)
 
 // DEAD CODE: Part 5,6,7, please remove this and go to Part 8!
 
