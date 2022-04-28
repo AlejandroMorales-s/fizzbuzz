979f81dde3ae621891cf496fdc2f2cb8550cbf6d
Alejandro <alejandrom.am33@gmail.com>
2022-04-28 00:05:57 -0500

    add: api

---
+++ api.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const ExplorerService = require("./lib/services/explorerService");
+const Reader = require("./lib/utils/reader");
+const explorers = Reader.readJsonFile("explorers.json");
+// Usando objeto express
+const express = require('express')
+// App de Express
+const app = express()
+// Puerto en que vamos a ver nuestra app: localhost: 3000
+const port = 3000
+// path inicial, responderá a la url localhost: 3000
+app.get('/', (req, res) => {
+res.send('Hello !')
+})
+app.get('/v1/explorers/:mission', (req, res) => {
+    const mission = req.params.mission
+    const explorersInNode = ExplorerService.filterByMission(explorers, mission)
+    res.send(explorersInNode)
+})
+app.get('/v1/explorers/amount/:mission', (req, res) => {
+    const mission = req.params.mission
+    const amountOfExplorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, mission)
+    res.send(amountOfExplorersInNode)
+})
+app.get('/v1/explorers/usernames/:mission', (req, res) => {
+    const mission = req.params.mission
+    const usernamesInNode = ExplorerService.getExporersUsernameByMission(explorers, mission)
+    res.send(usernamesInNode)
+})
+// Con esto inicializamos esta app
+app.listen (port, () => {
+console.log(`Example app listening on port ${port}`)
+})
