fb0c9f22b71829f3e047a7d0cc477c02c2200194
Alejandro <alejandrom.am33@gmail.com>
2022-04-27 22:03:23 -0500

    fix: rutas de archivos

--- test/service/explorerService.test.js
+++ test/service/explorerService.test.js
@@ -1,6 +1,6 @@
-const Reader = require("./lib/utils/Reader");
+const Reader = require("./../../lib/utils/Reader");
 const explorers = Reader.readJsonFile("explorers.json");
-const ExplorerService = require("./lib/services/explorerService");
+const ExplorerService = require("./../../lib/services/explorerService");
 
 describe("Unit test for ExplorerService class", () => {
 
