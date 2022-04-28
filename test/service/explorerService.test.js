8ba12ee3fddc2ffada83727e46c5fe356d5ec1b9
Alejandro <alejandrom.am33@gmail.com>
2022-04-27 19:25:50 -0500

    add: test para obtener cantidad de explorers en una mision

--- test/service/explorerService.test.js
+++ test/service/explorerService.test.js
@@ -8,4 +8,9 @@ describe("Unit test for ExplorerService class", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode[0].mission).toBe("node");
     });
+
+    test("Get amount of explorers by mission", () => {
+        const amountOfExplorersInNode = explorers.filter((explorer) => explorer.mission == "node").length;
+        expect(amountOfExplorersInNode).toBe(10);
+    })
 });
