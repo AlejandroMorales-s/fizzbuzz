acdcaaa0072ce03231b607332a970636d5ccecfa
Alejandro <alejandrom.am33@gmail.com>
2022-04-27 19:42:57 -0500

    add: test para clase Reader

---
+++ test/utils/reader.test.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const Reader = require("../../lib/utils/Reader");
+
+describe("Unit test for Reader class", () => {
+    test("Read json file", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+        expect(explorers).not.toBe(false);
+    });
+});
