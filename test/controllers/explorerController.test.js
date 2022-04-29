a310c5b58ec7ff4a07993603f4c9e81b6ac964e7
Alejandro <alejandrom.am33@gmail.com>
2022-04-29 16:07:43 -0500

    add: linter

--- test/controllers/explorerController.test.js
+++ test/controllers/explorerController.test.js
@@ -1,17 +1,17 @@
-const ExplorerController = require('./../../lib/controllers/explorerController');
+const ExplorerController = require("./../../lib/controllers/explorerController");
 
-describe('Prueba de unidad de la clase ExplorerController', () => {
-    test('Debe retornar un arreglo de exploradores', () => {
-        const explorers = ExplorerController.getExplorersByMission('node');
-        expect(explorers[0].mission).toBe('node');
+describe("Prueba de unidad de la clase ExplorerController", () => {
+    test("Debe retornar un arreglo de exploradores", () => {
+        const explorers = ExplorerController.getExplorersByMission("node");
+        expect(explorers[0].mission).toBe("node");
     });
-    test('Debe retornar un arreglo de exploradores', () => {
-        const explorers = ExplorerController.getExplorersUsernameByMission('node');
+    test("Debe retornar un arreglo de exploradores", () => {
+        const explorers = ExplorerController.getExplorersUsernameByMission("node");
         expect(Array.isArray(explorers)).toBe(true);
-        expect(explorers[0]).toBe('ajolonauta1');
+        expect(explorers[0]).toBe("ajolonauta1");
     });
-    test('Debe retornar un arreglo de exploradores en mision node', () => {
-        const explorers = ExplorerController.getExplorersAmountByMission('node');
+    test("Debe retornar un arreglo de exploradores en mision node", () => {
+        const explorers = ExplorerController.getExplorersAmountByMission("node");
         expect(explorers).toBe(10);
     });
-})
+});
